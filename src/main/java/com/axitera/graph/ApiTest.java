3ada6414fafb9857d19a647989c9b4bbb8119dc1
MuradHuseynov <muradace@gmail.com>
2022-11-23 16:51:15 +0100

    added apitest

--- src/main/java/com/axitera/graph/ApiTest.java
+++ src/main/java/com/axitera/graph/ApiTest.java
@@ -1,38 +1,57 @@
 package com.axitera.graph;
 
-import com.pdfcrowd.*;
-
-import java.io.*;
+import java.awt.Graphics;
+import java.awt.GraphicsEnvironment;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+//from  w  w w.  java  2  s . c o m
+import javax.imageio.ImageIO;
+import javax.swing.JEditorPane;
 
 public class ApiTest {
     //    public static void main(String[] args) throws IOException, Pdfcrowd.Error {
     public void myMethod() throws IOException {
-        try {
-            // create the API client instance
-            Pdfcrowd.HtmlToImageClient client =
-                    new Pdfcrowd.HtmlToImageClient("demo", "ce544b6ea52a5621fb9d55f8b542d14d");
+//        try {
+//            // create the API client instance
+//            Pdfcrowd.HtmlToImageClient client =
+//                    new Pdfcrowd.HtmlToImageClient("demo", "ce544b6ea52a5621fb9d55f8b542d14d");
+//
+//            // configure the conversion
+//            client.setOutputFormat("png");
+//
+//            // run the conversion and write the result to a file
+//            client.convertUrlToFile("http://google.de", "example.png");
+////            client.convertUrlToFile("templates/employees/list.html", "example.png");
+//            System.out.println("running" + client.getDebugLogUrl());
+//        } catch (Pdfcrowd.Error why) {
+//            // report the error
+//            System.err.println("Pdfcrowd Error: " + why);
+//
+//            // rethrow or handle the exception
+//            throw why;
+//        } catch (IOException why) {
+//            // report the error
+//            System.err.println("IO Error: " + why);
+//
+//            // rethrow or handle the exception
+//            throw why;
+//        }
 
-            // configure the conversion
-            client.setOutputFormat("png");
+            String html = "<h1>Hello, world.</h1>";
+            int width = 200, height = 100;
 
-            // run the conversion and write the result to a file
-            client.convertUrlToFile("http://google.de", "example.png");
-//            client.convertUrlToFile("templates/employees/list.html", "example.png");
-            System.out.println("running" + client.getDebugLogUrl());
-        } catch (Pdfcrowd.Error why) {
-            // report the error
-            System.err.println("Pdfcrowd Error: " + why);
+            BufferedImage image = GraphicsEnvironment.getLocalGraphicsEnvironment()
+                    .getDefaultScreenDevice().getDefaultConfiguration()
+                    .createCompatibleImage(width, height);
 
-            // rethrow or handle the exception
-            throw why;
-        } catch (IOException why) {
-            // report the error
-            System.err.println("IO Error: " + why);
+            Graphics graphics = image.createGraphics();
 
-            // rethrow or handle the exception
-            throw why;
-        }
+            JEditorPane jep = new JEditorPane("text/html", html);
+            jep.setSize(width, height);
+            jep.print(graphics);
 
+            ImageIO.write(image, "png", new File("Image.png"));
 
     }
 }
