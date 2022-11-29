931a64f5691384cfb70b4c3170e160d6917e5011
MuradHuseynov <muradace@gmail.com>
2022-11-29 09:37:37 +0100

    added padding

--- src/main/java/com/axitera/graph/controller/GraphController.java
+++ src/main/java/com/axitera/graph/controller/GraphController.java
@@ -9,7 +9,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.io.IOException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.Year;
@@ -27,19 +26,17 @@ public class GraphController {
 
     private final UserService userService;
 
-    record Request(LocalDateTime start, LocalDateTime end) {
-    }
+//    record Request(LocalDateTime start, LocalDateTime end) {
+//    }
 
 
     @RequestMapping("/list")
-    public String employeeList(Model model, @RequestParam(value = "date", required = false) String date) throws IOException {
-
-        String start;
-        String end;
+    public String employeeList(Model model, @RequestParam(value = "date", required = false) String date) {
+        String start, end;
         String[] result;
         int year = Year.now().getValue();
 
-        if (date ==null) {
+        if (date == null) {
             start = (year - 1) + "-01-01T00:00:00";
             end = year + "-12-28T23:59:00";
         } else {
@@ -48,7 +45,6 @@ public class GraphController {
             end = result[1] + "-12-28T23:59:00";
         }
 
-
         List<EmployeesView> users = userService.findAll(LocalDateTime.parse(start), LocalDateTime.parse(end));
 
         model.addAttribute("date", date);
@@ -56,52 +52,48 @@ public class GraphController {
         return "employees/listexamp";
     }
 
-//    @RequestMapping("/list/year")
-//    public String handlePostRequest(Model model) {
+
+//    @GetMapping("/add")
+//    public String addEmployee(Model model) {
 //
+//        // create model attribute to bind form data
+//        Users employee = new Users();
+//
+//        model.addAttribute("employee", employee);
+//
+//        return "employees/employee-form";
 //    }
-
-    @GetMapping("/add")
-    public String addEmployee(Model model) {
-
-        // create model attribute to bind form data
-        Users employee = new Users();
-
-        model.addAttribute("employee", employee);
-
-        return "employees/employee-form";
-    }
-
-    @GetMapping("/update")
-    public String updateEmployee(@RequestParam("employeeId") int id, Model model) {
-
-        // get the employee from the service
-        Users employee = userService.findById(id);
-
-        // set employee as a model attribute to pre-populate the form
-        model.addAttribute("employee", employee);
-
-        // send over to our form
-        return "employees/employee-form";
-    }
-
-    @PostMapping("/save")
-    public String saveEmployee(@ModelAttribute("employee") Users users) {
-
-        // save the employee
-        userService.save(users);
-
-        // use a redirect to prevent duplicated submissions
-        return "redirect:/employees/list";
-    }
-
-    @GetMapping("/delete")
-    public String delete(@RequestParam("employeeId") int id) {
-
-        // delete employee
-        userService.deleteById(id);
-
-        // return to list
-        return "redirect:/employees/list";
-    }
+//
+//    @GetMapping("/update")
+//    public String updateEmployee(@RequestParam("employeeId") int id, Model model) {
+//
+//        // get the employee from the service
+//        Users employee = userService.findById(id);
+//
+//        // set employee as a model attribute to pre-populate the form
+//        model.addAttribute("employee", employee);
+//
+//        // send over to our form
+//        return "employees/employee-form";
+//    }
+//
+//    @PostMapping("/save")
+//    public String saveEmployee(@ModelAttribute("employee") Users users) {
+//
+//        // save the employee
+//        userService.save(users);
+//
+//        // use a redirect to prevent duplicated submissions
+//        return "redirect:/employees/list";
+//    }
+//
+//    @GetMapping("/delete")
+//    public String delete(@RequestParam("employeeId") int id) {
+//
+//        // delete employee
+//        userService.deleteById(id);
+//
+//        // return to list
+//        return "redirect:/employees/list";
+//    }
 }
